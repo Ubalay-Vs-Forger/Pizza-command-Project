18862867a3791d9c9c5a7c737b6ab8b1c7a71ad1
Ubay201202 <ubaydullohhikmatx6@gmail.com>
2025-10-23 20:03:46 +0500

    asd

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import pzzza from '../assets/pizzashop.png'
+const Header = () => {
+  return (
+    <>
+      <header className='bg-black p-[30px]'>
+        <img src={pzzza} alt="" />
+      </header>
+    </>
+  )
+}
+
+export default Header
